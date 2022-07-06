94928404e67fac2fd9555e8c740f29360a62f075
pattliu <hanwenliu@brandeis.edu>
2022-07-06 15:23:00 -0400

    The format of plot and add the sticks to represent the excited states

--- ct_analysis.py
+++ ct_analysis.py
@@ -52,28 +52,33 @@ class Molecule:
             intensity3.append(0.0)
             intensity4.append(0.0)
 
-        #CT_Excited_State={} #{Symm: {Exc_num: CT_char}}
-        #Excited_States= {} # {Symm:{exc_num: [energy,
+        fig = plt.figure(figsize=(3.57,3.54),dpi=600)
+        plt.rcParams.update({'font.size': 10, 'font.weight':'medium'})
+        stick=[]
+        stick_h=[]
+        #CT_Excited_State={} #{Symm: {Exc_num: [metal_CT_char, mol_CT_char]}}
+        #Excited_States= {} # {Symm:{exc_num: [energy, osc_str, tdm_x, tdm_y, tdm_z]} } 
         for exc_num in self.Excited_States[symmetry]:
-            for i in range(len(intensity)):
-                energy_point = en_ax[i]
-                phi = gamma /(((energy_point - float(self.Excited_States[symmetry][exc_num][0]))**2 + gamma**2)* math.pi) 
-                if "E" in symmetry:
-                    intensity[i] += phi * float(self.Excited_States[symmetry][exc_num][1]) * 2     
-                    intensity2[i] += phi * float(self.CT_Excited_State[symmetry][exc_num][0]) * float(self.Excited_States[symmetry][exc_num][1]) * 2
-                    intensity3[i] += phi * float(self.CT_Excited_State[symmetry][exc_num][1]) * float(self.Excited_States[symmetry][exc_num][1]) * 2
-                    #intensity4[i] += phi * float(self.CT_Excited_State[symmetry][exc_num][1]) * float(self.Excited_States[symmetry][exc_num][1]) * 2
-                if "A" in symmetry or "S" in symmetry or "B" in symmetry:
-                    intensity[i] += phi * float(self.Excited_States[symmetry][exc_num][1]) * 1     
-                    intensity2[i] += phi * (float(self.CT_Excited_State[symmetry][exc_num][0])) * float(self.Excited_States[symmetry][exc_num][1]) * 1
-                    intensity3[i] += phi * (float(self.CT_Excited_State[symmetry][exc_num][1])) * float(self.Excited_States[symmetry][exc_num][1]) * 1
-                    #intensity4[i] += phi * abs(float(self.CT_Excited_State[symmetry][exc_num][1])) * float(self.Excited_States[symmetry][exc_num][1]) * 1
+            if float(self.Excited_States[symmetry][exc_num][0]) < max_energy:
+                stick.append(float(self.Excited_States[symmetry][exc_num][0]))
+                stick_h.append(float(self.Excited_States[symmetry][exc_num][1]))
+                for i in range(len(intensity)):
+                    energy_point = en_ax[i]
+                    phi = gamma /(((energy_point - float(self.Excited_States[symmetry][exc_num][0]))**2 + gamma**2)* math.pi) 
+                    if "E" in symmetry:
+                        intensity[i] += phi * float(self.Excited_States[symmetry][exc_num][1]) * 2     
+                        intensity2[i] += phi * float(self.CT_Excited_State[symmetry][exc_num][0]) * float(self.Excited_States[symmetry][exc_num][1]) * 2
+                        intensity3[i] += phi * float(self.CT_Excited_State[symmetry][exc_num][1]) * float(self.Excited_States[symmetry][exc_num][1]) * 2
+                        #intensity4[i] += phi * float(self.CT_Excited_State[symmetry][exc_num][1]) * float(self.Excited_States[symmetry][exc_num][1]) * 2
+                    if "A" in symmetry or "S" in symmetry or "B" in symmetry:
+                        intensity[i] += phi * float(self.Excited_States[symmetry][exc_num][1]) * 1     
+                        intensity2[i] += phi * (float(self.CT_Excited_State[symmetry][exc_num][0])) * float(self.Excited_States[symmetry][exc_num][1]) * 1
+                        intensity3[i] += phi * (float(self.CT_Excited_State[symmetry][exc_num][1])) * float(self.Excited_States[symmetry][exc_num][1]) * 1
+                        #intensity4[i] += phi * abs(float(self.CT_Excited_State[symmetry][exc_num][1])) * float(self.Excited_States[symmetry][exc_num][1]) * 1
         #print(f"intensity {intensity}")
-        fig = plt.figure(figsize=(12,10))
-        plt.rcParams.update({'font.size': 38, 'font.weight':'bold'})
         ax = fig.add_subplot(111)
-        #ax2 = ax.twinx()
-
+        ax2 = ax.twinx()
+        ax2.vlines(x = stick, ymin = 0, ymax = stick_h,linewidth=0.5)    
         ax.plot(en_ax, intensity,'-b',label="total abs")
         ax.set_xlabel("Energy")
        # ax.set_ylabel("total")
@@ -82,7 +87,10 @@ class Molecule:
         ax.plot(en_ax, intensity3,'-g', label="mol ct")
         #ax.plot(en_ax, intensity4,'-m', label="abs ct")
         ax.set_ylabel("Intensity")
-        plt.legend(prop={'size': 15})
+        ax.set_ylim(bottom=0)
+        ax2.set_ylim(ymin=0,ymax=max(stick_h)*2)
+        ax.legend(prop={'size': 8})
+        plt.tight_layout()
         #plt.show()
         plt.savefig(f"{plot_name}.png")
 
